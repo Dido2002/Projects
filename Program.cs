008d70825984f9d4c998c1e23cdd28e16d9c1d7e
Dido <100509456+Dido2002@users.noreply.github.com>
2022-08-15 22:04:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 
-namespace CourseWork375_2
+namespace CourseWorkTest2_3_I
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-            for (int i = 0; i <= 7; i++)
-            {
-                int number = int.Parse(Console.ReadLine());
-                if (number < 0 || number > 200) 
-                {
-                    sum += number;
-                }
-            }
-            Console.WriteLine($"The sum is:{sum}");
+           
         }
     }
 }
